739789e50b8963f71a916c1e76a808a0ee374c76
b2bananaman-pixel <b2bananaman@gmail.com>
2026-08-14 22:11:04 +0900

    Add top page authentication link tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,4 +5,12 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     get home_index_url
     assert_response :success
   end
+  test "新規登録とログインへのリンクが表示される" do
+    get home_index_url
+
+    assert_response :success
+    assert_select "a", text: "新規登録"
+    assert_select "a", text: "ログイン"
+  end
+
 end
