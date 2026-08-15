d7ec0cee7be5865966f7ee42e371298ff0b2a8b9
b2bananaman-pixel <b2bananaman@gmail.com>
2026-08-15 22:01:04 +0900

    Fix lint issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :practice_themes, only: [:index, :show]
+  resources :practice_themes, only: [ :index, :show ]
   devise_for :users
 
   get "home/index"
