3abe9878dc45e45407d951ac5cc253f5df2e332c
b2bananaman-pixel <b2bananaman@gmail.com>
2026-08-24 22:16:00 +0900

    Fix Render host authorization

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,6 +101,7 @@ Rails.application.configure do
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
   config.hosts << ENV["RENDER_EXTERNAL_HOSTNAME"] if ENV["RENDER_EXTERNAL_HOSTNAME"].present?
+  config.hosts << "smilecoach-projects.onrender.com"
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
